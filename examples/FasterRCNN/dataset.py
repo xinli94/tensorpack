9eb8230dcaa8fd4fdead0f91139399f38b55c9fc
Xin Li <xinli.94@hotmail.com>
2019-01-08 16:45:30 -0800

    Add custom dataset support back

--- examples/FasterRCNN/dataset.py
+++ examples/FasterRCNN/dataset.py
@@ -14,6 +14,34 @@ from config import config as cfg
 
 __all__ = ['COCODetection', 'DetectionDataset']
 
+class _COCOMeta(object):
+    # handle the weird (but standard) split of train and val
+    INSTANCE_TO_BASEDIR = {
+        'valminusminival2014': 'val2014',
+        'minival2014': 'val2014',
+    }
+
+    def valid(self):
+        return hasattr(self, 'cat_names')
+
+    def create(self, cat_ids, cat_names):
+        """
+        cat_ids: list of ids
+        cat_names: list of names
+        """
+        assert not self.valid()
+        assert len(cat_ids) == cfg.DATA.NUM_CATEGORY and len(cat_names) == cfg.DATA.NUM_CATEGORY
+        self.cat_names = cat_names
+        self.class_names = ['BG'] + self.cat_names
+
+        # background has class id of 0
+        self.category_id_to_class_id = {
+            v: i + 1 for i, v in enumerate(cat_ids)}
+        self.class_id_to_category_id = {
+            v: k for k, v in self.category_id_to_class_id.items()}
+        cfg.DATA.CLASS_NAMES = self.class_names
+
+COCOMeta = _COCOMeta()
 
 class COCODetection(object):
     # handle the weird (but standard) split of train and val
@@ -22,18 +50,10 @@ class COCODetection(object):
         'minival2014': 'val2014',
     }
 
-    COCO_id_to_category_id = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: 11, 13: 12, 14: 13, 15: 14, 16: 15, 17: 16, 18: 17, 19: 18, 20: 19, 21: 20, 22: 21, 23: 22, 24: 23, 25: 24, 27: 25, 28: 26, 31: 27, 32: 28, 33: 29, 34: 30, 35: 31, 36: 32, 37: 33, 38: 34, 39: 35, 40: 36, 41: 37, 42: 38, 43: 39, 44: 40, 46: 41, 47: 42, 48: 43, 49: 44, 50: 45, 51: 46, 52: 47, 53: 48, 54: 49, 55: 50, 56: 51, 57: 52, 58: 53, 59: 54, 60: 55, 61: 56, 62: 57, 63: 58, 64: 59, 65: 60, 67: 61, 70: 62, 72: 63, 73: 64, 74: 65, 75: 66, 76: 67, 77: 68, 78: 69, 79: 70, 80: 71, 81: 72, 82: 73, 84: 74, 85: 75, 86: 76, 87: 77, 88: 78, 89: 79, 90: 80}  # noqa
-    """
-    Mapping from the incontinuous COCO category id to an id in [1, #category]
-    """
-
-    class_names = [
-        "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]  # noqa
-
     def __init__(self, basedir, name):
         self.name = name
         self._imgdir = os.path.realpath(os.path.join(
-            basedir, self._INSTANCE_TO_BASEDIR.get(name, name)))
+            basedir, COCOMeta.INSTANCE_TO_BASEDIR.get(name, name)))
         assert os.path.isdir(self._imgdir), self._imgdir
         annotation_file = os.path.join(
             basedir, 'annotations/instances_{}.json'.format(name))
@@ -41,7 +61,37 @@ class COCODetection(object):
 
         from pycocotools.coco import COCO
         self.coco = COCO(annotation_file)
+
+        # initialize the meta
+        cat_ids = self.coco.getCatIds()
+        cat_names = [c['name'] for c in self.coco.loadCats(cat_ids)]
+        if not COCOMeta.valid():
+            COCOMeta.create(cat_ids, cat_names)
+        else:
+            assert COCOMeta.cat_names == cat_names
+
         logger.info("Instances loaded from {}.".format(annotation_file))
+
+    # COCO_id_to_category_id = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 11: 11, 13: 12, 14: 13, 15: 14, 16: 15, 17: 16, 18: 17, 19: 18, 20: 19, 21: 20, 22: 21, 23: 22, 24: 23, 25: 24, 27: 25, 28: 26, 31: 27, 32: 28, 33: 29, 34: 30, 35: 31, 36: 32, 37: 33, 38: 34, 39: 35, 40: 36, 41: 37, 42: 38, 43: 39, 44: 40, 46: 41, 47: 42, 48: 43, 49: 44, 50: 45, 51: 46, 52: 47, 53: 48, 54: 49, 55: 50, 56: 51, 57: 52, 58: 53, 59: 54, 60: 55, 61: 56, 62: 57, 63: 58, 64: 59, 65: 60, 67: 61, 70: 62, 72: 63, 73: 64, 74: 65, 75: 66, 76: 67, 77: 68, 78: 69, 79: 70, 80: 71, 81: 72, 82: 73, 84: 74, 85: 75, 86: 76, 87: 77, 88: 78, 89: 79, 90: 80}  # noqa
+    # """
+    # Mapping from the incontinuous COCO category id to an id in [1, #category]
+    # """
+
+    # class_names = [
+    #     "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]  # noqa
+
+    # def __init__(self, basedir, name):
+    #     self.name = name
+    #     self._imgdir = os.path.realpath(os.path.join(
+    #         basedir, self._INSTANCE_TO_BASEDIR.get(name, name)))
+    #     assert os.path.isdir(self._imgdir), self._imgdir
+    #     annotation_file = os.path.join(
+    #         basedir, 'annotations/instances_{}.json'.format(name))
+    #     assert os.path.isfile(annotation_file), annotation_file
+
+    #     from pycocotools.coco import COCO
+    #     self.coco = COCO(annotation_file)
+    #     logger.info("Instances loaded from {}.".format(annotation_file))
 
     # https://github.com/cocodataset/cocoapi/blob/master/PythonAPI/pycocoEvalDemo.ipynb
     def print_coco_metrics(self, json_file):
@@ -151,7 +201,8 @@ class COCODetection(object):
         # all geometrically-valid boxes are returned
         boxes = np.asarray([obj['bbox'] for obj in valid_objs], dtype='float32')  # (n, 4)
         cls = np.asarray([
-            self.COCO_id_to_category_id[obj['category_id']]
+            # self.COCO_id_to_category_id[obj['category_id']]
+            COCOMeta.category_id_to_class_id[obj['category_id']]
             for obj in valid_objs], dtype='int32')  # (n,)
         is_crowd = np.asarray([obj['iscrowd'] for obj in valid_objs], dtype='int8')
 
@@ -191,9 +242,13 @@ class DetectionDataset(object):
         This function is responsible for setting the dataset-specific
         attributes in both cfg and self.
         """
-        self.num_category = cfg.DATA.NUM_CATEGORY = 80
+        self.num_category = cfg.DATA.NUM_CATEGORY# = 80
         self.num_classes = self.num_category + 1
-        self.class_names = cfg.DATA.CLASS_NAMES = ["BG"] + COCODetection.class_names
+        self.class_names = cfg.DATA.CLASS_NAMES# = ["BG"] + COCODetection.class_names
+        print('==> self.num_category', self.num_category)
+        print('==> self.num_classes', self.num_classes)
+        print('==> self.class_names', self.class_names)
+
         assert len(self.class_names) == self.num_classes
 
     def load_training_roidbs(self, names):
@@ -259,7 +314,9 @@ class DetectionDataset(object):
         Returns:
             dict: the evaluation results.
         """
-        continuous_id_to_COCO_id = {v: k for k, v in COCODetection.COCO_id_to_category_id.items()}
+        # continuous_id_to_COCO_id = {v: k for k, v in COCODetection.COCO_id_to_category_id.items()}
+        continuous_id_to_COCO_id = {v: k for k, v in COCOMeta.category_id_to_class_id.items()}
+
         for res in results:
             # convert to COCO's incontinuous category id
             res['category_id'] = continuous_id_to_COCO_id[res['category_id']]
