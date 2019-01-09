e7b60f5eebb23bca6661d6d5c1e627e24e86247e
Xin Li <xinli.94@hotmail.com>
2019-01-08 16:41:02 -0800

    wip: [inference script] moves forward with updated orig repo

--- examples/FasterRCNN/inference.py
+++ examples/FasterRCNN/inference.py
@@ -14,29 +14,44 @@ from tensorpack.utils.utils import get_tqdm_kwargs
 from common import DataFromListOfDict
 from config import config as cfg
 from config import finalize_configs
-from eval import DetectionResult, detect_one_image, print_coco_metrics
-from train import ResNetC4Model, ResNetFPNModel
+from dataset import DetectionDataset
+from eval import DetectionResult, predict_image
+# from eval import DetectionResult, predict_image#, multithread_predict_dataflow #, detect_one_image, print_coco_metrics
+from train import ResNetC4Model, ResNetFPNModel, do_visualize, do_predict
 
-from coco import COCODetection, COCOMeta
+# from coco import COCODetection, COCOMeta
 
 def _offline_evaluate(pred_config, output_file):
+    # num_gpu = cfg.TRAIN.NUM_GPUS
+    # graph_funcs = MultiTowerOfflinePredictor(
+    #     pred_config, list(range(num_gpu))).get_predictors()
+    # predictors = []
+    # dataflows = []
+    # for k in range(num_gpu):
+    #     predictors.append(lambda img,
+    #                       pred=graph_funcs[k]: detect_one_image(img, pred))
+    #     dataflows.append(_get_eval_dataflow(shard=k, num_shards=num_gpu))
+    # if num_gpu > 1:
+    #     all_results = _multithread_eval_coco(dataflows, predictors)
+    # else:
+    #     all_results = _eval_coco(dataflows[0], predictors[0])
+
+    # with open(output_file + '.json', 'w+') as f:
+    #     json.dump(all_results, f)
+    # print_coco_metrics(output_file + '.json')
+
     num_gpu = cfg.TRAIN.NUM_GPUS
     graph_funcs = MultiTowerOfflinePredictor(
         pred_config, list(range(num_gpu))).get_predictors()
-    predictors = []
-    dataflows = []
-    for k in range(num_gpu):
-        predictors.append(lambda img,
-                          pred=graph_funcs[k]: detect_one_image(img, pred))
-        dataflows.append(_get_eval_dataflow(shard=k, num_shards=num_gpu))
-    if num_gpu > 1:
-        all_results = _multithread_eval_coco(dataflows, predictors)
-    else:
-        all_results = _eval_coco(dataflows[0], predictors[0])
 
-    with open(output_file + '.json', 'w+') as f:
-        json.dump(all_results, f)
-    print_coco_metrics(output_file + '.json')
+    for dataset in cfg.DATA.VAL:
+        logger.info("Evaluating {} ...".format(dataset))
+        dataflows = [
+            _get_eval_dataflow(dataset, shard=k, num_shards=num_gpu)
+            for k in range(num_gpu)]
+        all_results = _multithread_predict_dataflow(dataflows, graph_funcs)
+        output = output_file + '-' + dataset
+        DetectionDataset().eval_or_save_inference_results(all_results, dataset, output)
 
     records = []
     for res in all_results:
@@ -49,17 +64,19 @@ def _offline_evaluate(pred_config, output_file):
     records_df = pd.DataFrame.from_records(records, columns=['path', 'timestamp', 'width', 'height', 'left', 'top', 'right', 'bottom', 'score', 'class'])
     records_df.to_csv(output_file + '.csv', index=False, header=False)
 
-def _get_eval_dataflow(shard=0, num_shards=1):
+def _get_eval_dataflow(name, shard=0, num_shards=1):
     """
     Args:
+        name (str): name of the dataset to evaluate
         shard, num_shards: to get subset of evaluation data
     """
-    roidbs = COCODetection.load_many(cfg.DATA.BASEDIR, cfg.DATA.VAL, add_gt=False)
+    roidbs = DetectionDataset().load_inference_roidbs(name)
+
     num_imgs = len(roidbs)
     img_per_shard = num_imgs // num_shards
     img_range = (shard * img_per_shard, (shard + 1) * img_per_shard if shard + 1 < num_shards else num_imgs)
 
-    # no filter for training
+    # Note(xin): we need the image path
     ds = DataFromListOfDict(roidbs[img_range[0]: img_range[1]], ['file_name', 'id', 'file_name'])
 
     def f(fname):
@@ -70,29 +87,36 @@ def _get_eval_dataflow(shard=0, num_shards=1):
     # Evaluation itself may be multi-threaded, therefore don't add prefetch here.
     return ds
 
-# Note(xin): overwrite _eval_coco
-def _eval_coco(df, detect_func, tqdm_bar=None):
+def _predict_dataflow(df, model_func, tqdm_bar=None):
+    """
+    Args:
+        df: a DataFlow which produces (image, image_id)
+        model_func: a callable from the TF model.
+            It takes image and returns (boxes, probs, labels, [masks])
+        tqdm_bar: a tqdm object to be shared among multiple evaluation instances. If None,
+            will create a new one.
+
+    Returns:
+        list of dict, in the format used by
+        `DetectionDataset.eval_or_save_inference_results`
+    """
     df.reset_state()
     all_results = []
-    # tqdm is not quite thread-safe: https://github.com/tqdm/tqdm/issues/323
-    with ExitStack() as stack:
-        if tqdm_bar is None:
-            tqdm_bar = stack.enter_context(
-                tqdm.tqdm(total=df.size(), **get_tqdm_kwargs()))
-        for img, img_id, img_path in df:
-            results = detect_func(img)
-            for r in results:
-                box = r.box
-                cat_id = COCOMeta.class_id_to_category_id[r.class_id]
-                box[2] -= box[0]
-                box[3] -= box[1]
 
+    with ExitStack() as stack:
+        # tqdm is not quite thread-safe: https://github.com/tqdm/tqdm/issues/323
+        if tqdm_bar is None:
+            tqdm_bar = stack.enter_context(get_tqdm(total=df.size()))
+        for img, img_id, img_path in df:
+            results = predict_image(img, model_func)
+            for r in results:
                 res = {
+                    # Note(xin): add `image_path` and `label`
                     'image_path': img_path,
                     'image_id': img_id,
-                    'category_id': cat_id,
-                    'label': cfg.DATA.CLASS_NAMES[cat_id],
-                    'bbox': list(map(lambda x: round(float(x), 3), box)),
+                    'category_id': r.class_id,
+                    'label': cfg.DATA.CLASS_NAMES[r.class_id],
+                    'bbox': list(r.box),
                     'score': round(float(r.score), 4),
                 }
 
@@ -107,27 +131,108 @@ def _eval_coco(df, detect_func, tqdm_bar=None):
     return all_results
 
 
-def _multithread_eval_coco(dataflows, detect_funcs):
-    num_worker = len(dataflows)
-    assert len(dataflows) == len(detect_funcs)
+def _multithread_predict_dataflow(dataflows, model_funcs):
+    """
+    Running multiple `predict_dataflow` in multiple threads, and aggregate the results.
+
+    Args:
+        dataflows: a list of DataFlow to be used in :func:`predict_dataflow`
+        model_funcs: a list of callable to be used in :func:`predict_dataflow`
+
+    Returns:
+        list of dict, in the format used by
+        `DetectionDataset.eval_or_save_inference_results`
+    """
+    num_worker = len(model_funcs)
+    assert len(dataflows) == num_worker
+    if num_worker == 1:
+        return _predict_dataflow(dataflows[0], model_funcs[0])
     with ThreadPoolExecutor(max_workers=num_worker, thread_name_prefix='EvalWorker') as executor, \
             tqdm.tqdm(total=sum([df.size() for df in dataflows])) as pbar:
         futures = []
-        for dataflow, pred in zip(dataflows, detect_funcs):
-            futures.append(executor.submit(_eval_coco, dataflow, pred, pbar))
+        for dataflow, pred in zip(dataflows, model_funcs):
+            futures.append(executor.submit(_predict_dataflow, dataflow, pred, pbar))
         all_results = list(itertools.chain(*[fut.result() for fut in futures]))
         return all_results
+
+# def _get_eval_dataflow(shard=0, num_shards=1):
+#     """
+#     Args:
+#         shard, num_shards: to get subset of evaluation data
+#     """
+#     roidbs = COCODetection.load_many(cfg.DATA.BASEDIR, cfg.DATA.VAL, add_gt=False)
+#     num_imgs = len(roidbs)
+#     img_per_shard = num_imgs // num_shards
+#     img_range = (shard * img_per_shard, (shard + 1) * img_per_shard if shard + 1 < num_shards else num_imgs)
+
+#     # no filter for training
+#     ds = DataFromListOfDict(roidbs[img_range[0]: img_range[1]], ['file_name', 'id', 'file_name'])
+
+#     def f(fname):
+#         im = cv2.imread(fname, cv2.IMREAD_COLOR)
+#         assert im is not None, fname
+#         return im
+#     ds = MapDataComponent(ds, f, 0)
+#     # Evaluation itself may be multi-threaded, therefore don't add prefetch here.
+#     return ds
+
+# # Note(xin): overwrite _eval_coco
+# def _eval_coco(df, detect_func, tqdm_bar=None):
+#     df.reset_state()
+#     all_results = []
+#     # tqdm is not quite thread-safe: https://github.com/tqdm/tqdm/issues/323
+#     with ExitStack() as stack:
+#         if tqdm_bar is None:
+#             tqdm_bar = stack.enter_context(
+#                 tqdm.tqdm(total=df.size(), **get_tqdm_kwargs()))
+#         for img, img_id, img_path in df:
+#             results = detect_func(img)
+#             for r in results:
+#                 box = r.box
+#                 cat_id = COCOMeta.class_id_to_category_id[r.class_id]
+#                 box[2] -= box[0]
+#                 box[3] -= box[1]
+
+#                 res = {
+#                     'image_path': img_path,
+#                     'image_id': img_id,
+#                     'category_id': cat_id,
+#                     'label': cfg.DATA.CLASS_NAMES[cat_id],
+#                     'bbox': list(map(lambda x: round(float(x), 3), box)),
+#                     'score': round(float(r.score), 4),
+#                 }
+
+#                 # also append segmentation to results
+#                 if r.mask is not None:
+#                     rle = cocomask.encode(
+#                         np.array(r.mask[:, :, None], order='F'))[0]
+#                     rle['counts'] = rle['counts'].decode('ascii')
+#                     res['segmentation'] = rle
+#                 all_results.append(res)
+#             tqdm_bar.update(1)
+#     return all_results
+
+
+# def _multithread_eval_coco(dataflows, detect_funcs):
+#     num_worker = len(dataflows)
+#     assert len(dataflows) == len(detect_funcs)
+#     with ThreadPoolExecutor(max_workers=num_worker, thread_name_prefix='EvalWorker') as executor, \
+#             tqdm.tqdm(total=sum([df.size() for df in dataflows])) as pbar:
+#         futures = []
+#         for dataflow, pred in zip(dataflows, detect_funcs):
+#             futures.append(executor.submit(_eval_coco, dataflow, pred, pbar))
+#         all_results = list(itertools.chain(*[fut.result() for fut in futures]))
+#         return all_results
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--load', required=True, help='load a model for evaluation or training. Can overwrite BACKBONE.WEIGHTS')
-    # parser.add_argument('--logdir', help='log directory', default='train_log/maskrcnn')
-    # parser.add_argument('--visualize', action='store_true', help='visualize intermediate results')
+    parser.add_argument('--visualize', action='store_true', help='visualize intermediate results')
     parser.add_argument('--evaluate', default='xin', help="Run evaluation on COCO. "
                                            "This argument is the name of the output csv/json evaluation file")
-    # parser.add_argument('--predict', help="Run prediction on a given image. "
-                                          # "This argument is the path to the input image file")
+    parser.add_argument('--predict', help="Run prediction on a given image. "
+                                          "This argument is the path to the input image file")
     parser.add_argument('--config', help="A list of KEY=VALUE to overwrite those defined in config.py",
                         nargs='+')
 
@@ -146,3 +251,8 @@ if __name__ == '__main__':
         output_names=MODEL.get_inference_tensor_names()[1])
 
     _offline_evaluate(predcfg, args.evaluate)
+
+    if args.predict:
+        do_predict(OfflinePredictor(predcfg), args.predict)
+    if args.visualize:
+        do_visualize(MODEL, args.load)
